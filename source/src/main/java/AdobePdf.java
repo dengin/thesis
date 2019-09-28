71478327255963a24ea6e4f1b6a1fe398384f3ad
dengin <engindemircioglu@hotmail.com>
2019-09-28 23:07:49 +0300

    Sample code is added for reading hyperlinked words from pdf file

--- source/src/main/java/AdobePdf.java
+++ source/src/main/java/AdobePdf.java
@@ -2,14 +2,17 @@ import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDResources;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.interactive.action.PDAction;
 import org.apache.pdfbox.pdmodel.interactive.action.PDActionURI;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotation;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 
 import javax.imageio.ImageIO;
+import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -23,7 +26,7 @@ import java.util.List;
  */
 public class AdobePdf
 {
-    private static final String FILE_NAME = "test3.pdf";
+    private static final String FILE_NAME = "test2.pdf";
 
     public static void main(String[] args)
     {
@@ -33,6 +36,8 @@ public class AdobePdf
         readAnnotationsFromPdf();
         System.out.println("\n########################## Save Images From Pdf File ##########################");
         readImagesFromPdf();
+        System.out.println("\n########################## Read Hyperlinked Words From Pdf File ##########################");
+        readHyperlinkedWordsFromPdf();
     }
 
     private static void readTextFromPdf()
@@ -124,6 +129,71 @@ public class AdobePdf
                         parentDir.mkdirs();
                         ImageIO.write(((org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject) o).getImage(), "png", file);
                         j++;
+                    }
+                }
+            }
+            document.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static void readHyperlinkedWordsFromPdf()
+    {
+        ClassLoader classLoader = new AdobePdf().getClass().getClassLoader();
+        try
+        {
+            URL url = classLoader.getResource(FILE_NAME);
+
+            PDDocument document = PDDocument.load(new File(url.getFile()));
+
+            for (int i = 0; i < document.getNumberOfPages(); i++)
+            {
+                PDPage pdfpage = document.getPage(i);
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                List<PDAnnotation> annotations = pdfpage.getAnnotations();
+                //first setup text extraction regions
+                for (int j = 0; j < annotations.size(); j++)
+                {
+                    PDAnnotation annot = annotations.get(j);
+                    if (annot instanceof PDAnnotationLink)
+                    {
+                        PDAnnotationLink link = (PDAnnotationLink) annot;
+                        PDRectangle rect = link.getRectangle();
+                        //need to reposition link rectangle to match text space
+                        float x = rect.getLowerLeftX();
+                        float y = rect.getUpperRightY();
+                        float width = rect.getWidth();
+                        float height = rect.getHeight();
+                        int rotation = pdfpage.getRotation();
+                        if (rotation == 0)
+                        {
+                            PDRectangle pageSize = pdfpage.getMediaBox();
+                            y = pageSize.getHeight() - y;
+                        }
+                        else if (rotation == 90)
+                        {
+                        }
+                        Rectangle2D.Float awtRect = new Rectangle2D.Float(x, y, width, height);
+                        stripper.addRegion("" + j, awtRect);
+                    }
+                }
+                stripper.extractRegions(pdfpage);
+                for (int j = 0; j < annotations.size(); j++)
+                {
+                    PDAnnotation annot = annotations.get(j);
+                    if (annot instanceof PDAnnotationLink)
+                    {
+                        PDAnnotationLink link = (PDAnnotationLink) annot;
+                        PDAction action = link.getAction();
+                        String urlText = stripper.getTextForRegion("" + j);
+                        if (action instanceof PDActionURI)
+                        {
+                            PDActionURI uri = (PDActionURI) action;
+                            System.out.println("Page " + (i + 1) + ":'" + urlText.trim() + "'=" + uri.getURI());
+                        }
                     }
                 }
             }
