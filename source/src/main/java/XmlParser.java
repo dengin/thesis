9cb47119be1a6562d59887ee12d0020f67962dea
dengin <engindemircioglu@hotmail.com>
2020-06-21 22:23:06 +0300

    Sadece kelimelerden olusan dosyalarin hazirlandigi bolum eklendi

--- source/src/main/java/XmlParser.java
+++ source/src/main/java/XmlParser.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.Locale;
 
 /**
  * User: dengin
@@ -32,11 +33,15 @@ import java.io.UnsupportedEncodingException;
  */
 public class XmlParser
 {
+    private static String FILE_DIR = "C:\\";
+
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, JAXBException
     {
-        dergiparkSozlukOlustur();
-        sobiadSozlukOlustur();
+//        dergiparkSozlukOlustur();
+//        sobiadSozlukOlustur();
 
+        dergiparkMakaleIcerikOlustur();
+        sobiadMakaleIcerikOlustur();
 
 //        JAXBContext context = JAXBContext.newInstance(Makaleler.class);
 //        Unmarshaller un = context.createUnmarshaller();
@@ -85,79 +90,24 @@ public class XmlParser
 //        }
     }
 
-    private static void sobiadSozlukOlustur() throws FileNotFoundException, UnsupportedEncodingException, SAXException, ParserConfigurationException, JAXBException
-    {
-        File xmlFile = new File("D:\\sobiad.txt");
-        InputStream inputStream= new FileInputStream(xmlFile);
-        InputStreamReader inputReader = new InputStreamReader(inputStream,"UTF-8");
-        InputSource inputSource = new InputSource(inputReader);
-        inputSource.setEncoding("UTF-8");
-
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
-        factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-        factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-
-        Source xmlSource = new SAXSource(factory.newSAXParser().getXMLReader(), inputSource);
-
-        JAXBContext jaxbContext = JAXBContext.newInstance(SobiadMakaleler.class);
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        SobiadMakaleler makaleler = (SobiadMakaleler) jaxbUnmarshaller.unmarshal(xmlSource);
-
-        sozlukOlusturSobiad(makaleler);
-    }
-
-    private static void sozlukOlusturSobiad(SobiadMakaleler makaleler)
-    {
-        try
-        {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\kelimeler_sobiad.txt"));
-
-            for (SobiadMakale makale : makaleler.getMakale())
-            {
-                if (makale.getSira() % 1000 < 3)
-                {
-                    System.out.println(makale.getSira());
-                }
-                String content = "";
-                if (makale.getBaslik() != null)
-                {
-                    content += metinEkle(makale.getBaslik());
-                }
-                if (makale.getOzet() != null)
-                {
-                    content += metinEkle(makale.getOzet());
-                }
-                if (makale.getAnahtarbilgileri() != null)
-                {
-                    for (SobiadAnahtarBilgileri anahtarBilgileri : makale.getAnahtarbilgileri())
-                    {
-                        content += metinEkle(anahtarBilgileri.getAnahtar());
-                    }
-                }
-                if (makale.getPdf() != null)
-                {
-                    content += metinEkle(makale.getPdf());
-                }
-                if (!content.isEmpty())
-                {
-                    writer.write(content.trim() + " ");
-                }
-            }
-            writer.close();
-
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
-
     private static void dergiparkSozlukOlustur() throws ParserConfigurationException, IOException, SAXException, JAXBException
     {
-        File xmlFile = new File("D:\\dergipark.txt");
-        InputStream inputStream= new FileInputStream(xmlFile);
-        InputStreamReader inputReader = new InputStreamReader(inputStream,"UTF-8");
+        dergiparkSozlukOlustur(getDergiparkMakaleler());
+    }
+
+    private static Makaleler getDergiparkMakaleler() throws FileNotFoundException, UnsupportedEncodingException, ParserConfigurationException, SAXException, JAXBException
+    {
+        File xmlFile = new File(FILE_DIR + "dergipark.txt");
+        Source xmlSource = getXmlSource(xmlFile);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Makaleler.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        return (Makaleler) jaxbUnmarshaller.unmarshal(xmlSource);
+    }
+
+    private static Source getXmlSource(File xmlFile) throws FileNotFoundException, UnsupportedEncodingException, ParserConfigurationException, SAXException
+    {
+        InputStream inputStream = new FileInputStream(xmlFile);
+        InputStreamReader inputReader = new InputStreamReader(inputStream, "UTF-8");
         InputSource inputSource = new InputSource(inputReader);
         inputSource.setEncoding("UTF-8");
 
@@ -166,20 +116,14 @@ public class XmlParser
         factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
         factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
 
-        Source xmlSource = new SAXSource(factory.newSAXParser().getXMLReader(), inputSource);
-
-        JAXBContext jaxbContext = JAXBContext.newInstance(Makaleler.class);
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        Makaleler makaleler = (Makaleler) jaxbUnmarshaller.unmarshal(xmlSource);
-
-        sozlukOlustur(makaleler);
+        return new SAXSource(factory.newSAXParser().getXMLReader(), inputSource);
     }
 
-    private static void sozlukOlustur(Makaleler makaleler)
+    private static void dergiparkSozlukOlustur(Makaleler makaleler)
     {
         try
         {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\kelimeler_dergipark.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_DIR + "kelimeler_dergipark.txt"));
 
             for (Makale makale : makaleler.getMakale())
             {
@@ -239,6 +183,26 @@ public class XmlParser
     private static String metinEkle(String text)
     {
         String temizMetin = text.replaceAll("[^a-zA-Z ıiüğşöçÜİĞŞÇÖ,.;:\"!?()\\-]", "");
+        temizMetin = temizMetin.toLowerCase(Locale.forLanguageTag("tr"));
+        temizMetin = temizMetin.replace("  ", " ");
+        temizMetin = temizMetin.replace("   ", " ");
+        temizMetin = temizMetin.replace("    ", " ");
+        temizMetin = temizMetin.replace("     ", " ");
+        temizMetin = temizMetin.replace("      ", " ");
+        temizMetin = temizMetin.replace("       ", " ");
+        temizMetin = temizMetin.replace("        ", " ");
+        temizMetin = temizMetin.replace("         ", " ");
+        temizMetin = temizMetin.replace("          ", " ");
+        temizMetin = temizMetin.replace("           ", " ");
+        temizMetin = temizMetin.replace("            ", " ");
+        temizMetin = temizMetin.replace("             ", " ");
+        temizMetin = temizMetin.replace("              ", " ");
+        temizMetin = temizMetin.replace("               ", " ");
+        temizMetin = temizMetin.replace("                ", " ");
+        temizMetin = temizMetin.replace("                 ", " ");
+        temizMetin = temizMetin.replace("                  ", " ");
+        temizMetin = temizMetin.replace("                   ", " ");
+        temizMetin = temizMetin.replace("                    ", " ");
         temizMetin = temizMetin.replace(",", " <COMMA> ");
         temizMetin = temizMetin.replace(".", " <PERIOD> ");
         temizMetin = temizMetin.replace(";", " <SEMICOLON> ");
@@ -250,6 +214,176 @@ public class XmlParser
         temizMetin = temizMetin.replace(")", " <RIGHT_PAREN> ");
         temizMetin = temizMetin.replace("-", " <HYPHENS> ");
         temizMetin = temizMetin.replace("\n", " <NEW_LINE> ");
-        return temizMetin.toLowerCase();
+        return temizMetin;
+    }
+
+    private static void sobiadSozlukOlustur() throws FileNotFoundException, UnsupportedEncodingException, SAXException, ParserConfigurationException, JAXBException
+    {
+        sozlukOlusturSobiad(getSobiadMakaleler());
+    }
+
+    private static SobiadMakaleler getSobiadMakaleler() throws FileNotFoundException, UnsupportedEncodingException, ParserConfigurationException, SAXException, JAXBException
+    {
+        File xmlFile = new File(FILE_DIR + "sobiad.txt");
+        Source xmlSource = getXmlSource(xmlFile);
+        JAXBContext jaxbContext = JAXBContext.newInstance(SobiadMakaleler.class);
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        return (SobiadMakaleler) jaxbUnmarshaller.unmarshal(xmlSource);
+    }
+
+    private static void sozlukOlusturSobiad(SobiadMakaleler makaleler)
+    {
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_DIR + "kelimeler_sobiad.txt"));
+
+            for (SobiadMakale makale : makaleler.getMakale())
+            {
+                if (makale.getSira() % 1000 < 3)
+                {
+                    System.out.println(makale.getSira());
+                }
+                String content = "";
+                if (makale.getBaslik() != null)
+                {
+                    content += metinEkle(makale.getBaslik());
+                }
+                if (makale.getOzet() != null)
+                {
+                    content += metinEkle(makale.getOzet());
+                }
+                if (makale.getAnahtarbilgileri() != null)
+                {
+                    for (SobiadAnahtarBilgileri anahtarBilgileri : makale.getAnahtarbilgileri())
+                    {
+                        content += metinEkle(anahtarBilgileri.getAnahtar());
+                    }
+                }
+                if (makale.getPdf() != null)
+                {
+                    content += metinEkle(makale.getPdf());
+                }
+                if (!content.isEmpty())
+                {
+                    writer.write(content.trim() + " ");
+                }
+            }
+            writer.close();
+
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static void dergiparkMakaleIcerikOlustur() throws FileNotFoundException, JAXBException, ParserConfigurationException, SAXException, UnsupportedEncodingException
+    {
+        icerikOlusturDergipark(getDergiparkMakaleler());
+    }
+
+    private static void icerikOlusturDergipark(Makaleler makaleler)
+    {
+        String adres = FILE_DIR + "icerik_dergipark.csv";
+
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(adres));
+            String content = "sira;makale_no;icerik\n";
+            writer.write(content);
+            int i = 1;
+            for (Makale makale : makaleler.getMakale())
+            {
+                content = "";
+                if (makale.getSira() % 1000 < 2)
+                {
+                    System.out.println(makale.getSira());
+                }
+                if (makale.getBaslik() != null)
+                {
+                    content += metinEkle(makale.getBaslik()) + " ";
+                }
+                if (makale.getOzetbilgisi() != null)
+                {
+                    content += metinEkle(makale.getOzetbilgisi()) + " ";
+                }
+                if (makale.getAnahtarbilgileri() != null)
+                {
+                    for (AnahtarBilgileri anahtarBilgileri : makale.getAnahtarbilgileri())
+                    {
+                        content += metinEkle(anahtarBilgileri.getAnahtar()) + " ";
+                    }
+                }
+                if (makale.getPdf() != null)
+                {
+                    content += metinEkle(makale.getPdf()) + " ";
+                }
+                if (!content.isEmpty() && !content.trim().equals(""))
+                {
+                    content = i++ + ";" + makale.getSira() + ";" + content.trim() + "\n";
+                    writer.write(content);
+                }
+            }
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sobiadMakaleIcerikOlustur() throws FileNotFoundException, JAXBException, ParserConfigurationException, SAXException, UnsupportedEncodingException
+    {
+        icerikOlusturSobiad(getSobiadMakaleler());
+    }
+
+    private static void icerikOlusturSobiad(SobiadMakaleler makaleler)
+    {
+        String adres = FILE_DIR + "icerik_sobiad.csv";
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(adres));
+            String content = "sira;makale_no;icerik\n";
+            writer.write(content.trim() + "\n");
+
+            int i = 1;
+            for (SobiadMakale makale : makaleler.getMakale())
+            {
+                content = "";
+                if (makale.getSira() % 1000 < 2)
+                {
+                    System.out.println(makale.getSira());
+                }
+                if (makale.getBaslik() != null)
+                {
+                    content += metinEkle(makale.getBaslik()) + " ";
+                }
+                if (makale.getOzet() != null)
+                {
+                    content += metinEkle(makale.getOzet()) + " ";
+                }
+                if (makale.getAnahtarbilgileri() != null)
+                {
+                    for (SobiadAnahtarBilgileri anahtarBilgileri : makale.getAnahtarbilgileri())
+                    {
+                        content += metinEkle(anahtarBilgileri.getAnahtar()) + " ";
+                    }
+                }
+                if (makale.getPdf() != null)
+                {
+                    content += metinEkle(makale.getPdf()) + " ";
+                }
+                if (!content.isEmpty() && !content.trim().equals(""))
+                {
+                    content = i++ + ";" + makale.getSira() + ";" + content.trim() + "\n";
+                    writer.write(content);
+                }
+            }
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
